cb2d6e87752f3895fc589c8e0c98b8cf6895f62e
awais-124 <awais14940@gmail.com>
2024-09-05 23:38:07 +0500

    day-one

--- src/admin/components/AddStudent/StudentViewModal.js
+++ src/admin/components/AddStudent/StudentViewModal.js
@@ -9,53 +9,74 @@ function StudentViewModal({ student, onClose }) {
           X
         </button>
         <h2>Student Details</h2>
-        <p>
-          <strong>Registration Number:</strong> {student.stdRegNumber}
-        </p>
-        <p>
-          <strong>First Name:</strong> {student.firstName}
-        </p>
-        <p>
-          <strong>Last Name:</strong> {student.lastName}
-        </p>
-        <p>
-          <strong>Father's Name:</strong> {student.fatherName}
-        </p>
-        <p>
-          <strong>Father's Occupation:</strong> {student.fatherOccupation}
-        </p>
-        <p>
-          <strong>Date of Birth:</strong> {student.dateOfBirth}
-        </p>
-        <p>
-          <strong>Section:</strong> {student.section}
-        </p>
-        <p>
-          <strong>Email:</strong> {student.email}
-        </p>
-        <p>
-          <strong>Contact Number:</strong> {student.contactNumber}
-        </p>
-        <p>
-          <strong>Gender:</strong> {student.gender}
-        </p>
-        <p>
-          <strong>CNIC:</strong> {student.cnic}
-        </p>
-        <p>
-          <strong>Semester:</strong> {student.semester}
-        </p>
-        <p>
-          <strong>Department:</strong> {student.department}
-        </p>
-        <p>
-          <strong>Program:</strong> {student.program}
-        </p>
-        <p>
-          <strong>Address:</strong> {student.address.houseNo},{' '}
-          {student.address.street}, {student.address.town},{' '}
-          {student.address.city}
-        </p>
+        <table className={classes.studentTable}>
+          <tbody>
+            <tr>
+              <td className={classes.label}>Registration Number:</td>
+              <td className={classes.value}>{student.stdRegNumber}</td>
+            </tr>
+            <tr>
+              <td className={classes.label}>Name:</td>
+              <td className={classes.value}>
+                {student.firstName + ' ' + student.lastName}
+              </td>
+            </tr>
+            <tr>
+              <td className={classes.label}>Father's Name:</td>
+              <td className={classes.value}>{student.fatherName}</td>
+            </tr>
+            <tr>
+              <td className={classes.label}>Father's Occupation:</td>
+              <td className={classes.value}>{student.fatherOccupation}</td>
+            </tr>
+            <tr>
+              <td className={classes.label}>Date of Birth:</td>
+              <td className={classes.value}>{student.dateOfBirth}</td>
+            </tr>
+            <tr>
+              <td className={classes.label}>Section:</td>
+              <td className={classes.value}>{student.section}</td>
+            </tr>
+            <tr>
+              <td className={classes.label}>Email:</td>
+              <td className={classes.value}>{student.email}</td>
+            </tr>
+            <tr>
+              <td className={classes.label}>Contact Number:</td>
+              <td className={classes.value}>{student.contactNumber}</td>
+            </tr>
+            <tr>
+              <td className={classes.label}>Gender:</td>
+              <td className={classes.value}>
+                {student.gender === 'M' ? 'Male' : 'Female'}
+              </td>
+            </tr>
+            <tr>
+              <td className={classes.label}>CNIC:</td>
+              <td className={classes.value}>{student.cnic}</td>
+            </tr>
+            <tr>
+              <td className={classes.label}>Semester:</td>
+              <td className={classes.value}>{student.semester}</td>
+            </tr>
+            <tr>
+              <td className={classes.label}>Department:</td>
+              <td className={classes.value}>{student.department}</td>
+            </tr>
+            <tr>
+              <td className={classes.label}>Program:</td>
+              <td className={classes.value}>{student.program}</td>
+            </tr>
+            <tr>
+              <td className={classes.label}>Address:</td>
+              <td className={classes.value}>
+                House No#{student.address.houseNo}, Street#
+                {student.address.street}, {student.address.town},{' '}
+                {student.address.city}
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   );
