8fabf1efd878f58b2d93e4a8e26a2cfff36b6d39
Muhammad Awais <awais14940@gmail.com>
2025-03-24 22:14:57 +0500

    tech

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,25 +1,14 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
-/* MINE */
 const firebaseConfig = {
-  apiKey: 'AIzaSyBfZdxN2Y0wxstM0KfEIZ4Ew5rAyajLh6A',
-  authDomain: 'student-portal-34800.firebaseapp.com',
-  projectId: 'student-portal-34800',
-  storageBucket: 'student-portal-34800.appspot.com',
-  messagingSenderId: '894551593062',
-  appId: '1:894551593062:web:94ad5dc02b4fdbab999da7',
+  apiKey: 'YOUR_API_KEY',
+  authDomain: 'YOUR_DOMAIN_NAME',
+  projectId: 'YOUR_PROJECT_ID',
+  storageBucket: '_YOUR_STORAGE_BUCKET',
+  messagingSenderId: 'YOUR_MESSENGER_ID',
+  appId: 'YOUR_APP_ID',
 };
-
-/* RANDOM */
-// const firebaseConfig = {
-//   apiKey: 'AIzaSyBFbrhz5hqo6aLPajiIRDF-Anc7hukB4Jc',
-//   authDomain: 'student-portal-7056c.firebaseapp.com',
-//   projectId: 'student-portal-7056c',
-//   storageBucket: 'student-portal-7056c.firebasestorage.app',
-//   messagingSenderId: '854974823090',
-//   appId: '1:854974823090:web:62c2ffde70b6817a5a67ab',
-// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
